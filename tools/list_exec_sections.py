8ffbe22b00b41fc7b1438744bf3a8717c694db78
poxyran <poxyrantab@gmail.com>
2013-07-26 14:47:37 -0300

    silly fix.

--- tools/list_exec_sections.py
+++ tools/list_exec_sections.py
@@ -15,5 +15,5 @@ elf = pylibelf.ELF(filename)
 
 for section in elf.ShdrTable:
 	if (section.sh_flags.value & elfconstants.ELF_SHF_FLAGS["SHF_EXECINSTR"]) == elfconstants.ELF_SHF_FLAGS["SHF_EXECINSTR"]:
-		print "[+] Section is marked as executable!"
+		print "[+] Section %s is marked as executable!" % section.sectionName
 
